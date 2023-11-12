6b214c6a5e4bacb412035db6bc82df27c938323d
Jonathan Lee <79165906+jsmlee123@users.noreply.github.com>
2023-11-12 01:42:16 -0500

    Test

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -43,7 +43,7 @@ function Kanbas() {
   };
 
 
-  const URL = "http://localhost:4000/api/courses";
+  const URL = "https://kanbas-node-server-app-j2bj.onrender.com/api/courses";
   const findAllCourses = async () => {
     const response = await axios.get(URL);
     setCourses(response.data);
